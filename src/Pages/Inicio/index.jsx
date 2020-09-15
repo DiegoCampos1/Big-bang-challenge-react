72721b62c576cbb738f33d0419cc484e0c5896e7
Diego Campos <diegodiko05@gmail.com>
2020-09-14 23:16:03 -0300

    Buttons laterais na pagina Inicial

--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './style.css';
-import elipse from '../../images/elipse.png';
+import arrow from '../../images/arrow.png';
 
 const Inicio = () => {
   return (
     <div>
       <div className="inicioContainer">
-        <img src={elipse} alt="elipse" />
+        <div className="elipse">O que é Namah?</div>
         <div className="baner">
           <h4>Lorem, ipsum dolor sit amet</h4>
           <p>Lorem, ipsum dolor sit amet</p>
           <p>Lorem, ipsum dolor sit amet</p>
         </div>
-        <img src={elipse} alt="elipse" />
+        <div className="elipseImg"><img src={arrow} alt=""/></div>
+
       </div>
     </div>
   );
