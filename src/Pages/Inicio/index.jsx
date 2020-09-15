191f998e3e30a977dba1f92bfa61cf7dcff8cd62
Diego Campos <diegodiko05@gmail.com>
2020-09-15 18:27:34 -0300

    Tela de inicio, com Flex box e Grid

--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -7,13 +7,40 @@ const Inicio = () => {
     <div>
       <div className="inicioContainer">
         <div className="elipse">O que é Namah?</div>
-        <div className="baner">
-          <h4>Lorem, ipsum dolor sit amet</h4>
-          <p>Lorem, ipsum dolor sit amet</p>
-          <p>Lorem, ipsum dolor sit amet</p>
-        </div>
-        <div className="elipseImg"><img src={arrow} alt=""/></div>
+        <div className="centralContainer">
+          <div className="baner">
+            <h4>Lorem, ipsum dolor sit amet</h4>
+            <p>Lorem, ipsum dolor sit amet</p>
+            <button className="buttonLorem" type="button">
+              LOREM IPSUM
+            </button>
+          </div>
+          <div className="bannersLinks">
+            <div className="biggerBannerYoga">
+              <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+              <a href="">VIAGEM</a>
+              <a href="">por Fulano de Tal</a>0{' '}
+            </div>
 
+            <div className="MediumBannerYoga">
+              <a href="">Namahcast #122</a>
+              <h3>Nome do episódio do podcast lorem ipsum dolor sit amet consectetur eli...</h3>
+            </div>
+            <div className="imersaoPicture">
+              <a href="">Curso - 04 ABR</a>
+              <h3>Imersão Vinyasa Flow</h3>
+            </div>
+            <div className="CondiciondorPicture">
+              <h3>Condicionador Namah</h3>
+              <a href="">compre agora</a>
+            </div>
+
+            <div className="banerShop"></div>
+          </div>
+        </div>
+        <div className="elipseImg">
+          <img src={arrow} alt="" />
+        </div>
       </div>
     </div>
   );
