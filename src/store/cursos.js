a4f6dc8267bcc39ae9a189b3fd56b8717e30acfb
Diego Campos <diegodiko05@gmail.com>
2020-09-16 20:05:14 -0300

    Inserindo os cursos e os podcasts

---
+++ src/store/cursos.js
@@ -0,0 +1,27 @@
+import yoga3 from '../images/yoga3.png';
+
+const cursos = [
+  {
+    picture: yoga3,
+    date: '04 Abr - 05 Abr',
+    titulo: 'Imersão Vinyasa Flow',
+    autor: 'Por Fernanda Cunha',
+    tempo: '2 dias de curso',
+  },
+  {
+    picture: yoga3,
+    date: '04 Abr - 05 Abr',
+    titulo: 'Imersão Vinyasa Flow',
+    autor: 'Por Fernanda Cunha',
+    tempo: '2 dias de curso',
+  },
+  {
+    picture: yoga3,
+    date: '04 Abr - 05 Abr',
+    titulo: 'Imersão Vinyasa Flow',
+    autor: 'Por Fernanda Cunha',
+    tempo: '2 dias de curso',
+  },
+];
+
+export default cursos;
---
+++ src/store/podcasts.js
@@ -0,0 +1,28 @@
+import yoga3 from '../images/yoga3.png';
+import yoga4 from '../images/yoga4.png';
+import yoga5 from '../images/yoga5.png';
+
+const podcasts = [
+  {
+    picture: yoga4,
+    numero: 'Namacast #123',
+    titulo:
+      'Título do podcast lorem ipsum dolor sit amet, consectetur elit tempor ut labore magna aliqua et dolore',
+    descricao:
+      'Descrição do episódio do podcast lorem ipsum dolor sit amet, consectetur elit tempor',
+  },
+  {
+    picture: yoga3,
+    date: '04 Abr - 05 Abr',
+    titulo: 'Imersão Vinyasa Flow',
+    autor: 'Por Fernanda Cunha',
+  },
+  {
+    picture: yoga5,
+    date: '04 Abr - 05 Abr',
+    titulo: 'Imersão Vinyasa Flow',
+    autor: 'Por Fernanda Cunha',
+  },
+];
+
+export default podcasts;
