947d89cd9bd57d4f2cc87b15a3759debfcb88c80
Diego Campos <diegodiko05@gmail.com>
2020-09-15 23:05:02 -0300

    Banner shop na tela de inicio

--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import arrow from '../../images/arrow.png';
+import produtosRecomendados from '../../store/products';
 
 const Inicio = () => {
   return (
@@ -18,24 +19,44 @@ const Inicio = () => {
           <div className="bannersLinks">
             <div className="biggerBannerYoga">
               <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
-              <a href="">VIAGEM</a>
-              <a href="">por Fulano de Tal</a>0{' '}
+              <a href="/">VIAGEM</a>
+              <a href="/">por Fulano de Tal</a>
             </div>
 
             <div className="MediumBannerYoga">
-              <a href="">Namahcast #122</a>
+              <a href="/">Namahcast #122</a>
               <h3>Nome do episódio do podcast lorem ipsum dolor sit amet consectetur eli...</h3>
             </div>
             <div className="imersaoPicture">
-              <a href="">Curso - 04 ABR</a>
+              <a href="/">Curso - 04 ABR</a>
               <h3>Imersão Vinyasa Flow</h3>
             </div>
             <div className="CondiciondorPicture">
               <h3>Condicionador Namah</h3>
-              <a href="">compre agora</a>
+              <a href="/">compre agora</a>
             </div>
-
-            <div className="banerShop"></div>
+            <div className="banerShop">
+              <div>
+                <p>Nossos especialistas</p>
+                <h5>Recomemendam</h5>
+              </div>
+              {produtosRecomendados.map((product) => (
+                <div>
+                  <img src={product.picture}></img>
+                  <p>{product.nome}</p>
+                  <p>{product.descricicao}</p>
+                  <p>{product.valor}</p>
+                </div>
+              ))}
+              {produtosRecomendados.map((product) => (
+                <div>
+                  <img src={product.picture}></img>
+                  <p>{product.nome}</p>
+                  <p>{product.descricicao}</p>
+                  <p>{product.valor}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className="elipseImg">
---
+++ src/store/products.js
@@ -0,0 +1,26 @@
+import camiseta from '../images/camiseta.png';
+import livro from '../images/livro.png';
+import tapete from '../images/tapete.png';
+
+const produtosRecomendados = [
+  {
+    nome: 'Livro Acenda Sua Luz',
+    descricicao: 'Carol Rache',
+    valor: 'R$ 54,00',
+    picture: livro,
+  },
+  {
+    nome: 'Camiseta Solidária',
+    descricicao: 'Tamanho Unico',
+    valor: 'R$ 149,00',
+    picture: camiseta,
+  },
+  {
+    nome: 'Tapete Viagem Pássaro',
+    descricicao: '',
+    valor: 'R$ 54,00',
+    picture: tapete,
+  },
+];
+
+export default produtosRecomendados;
