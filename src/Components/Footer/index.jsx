4826746287cbae4af99013364d9e3bc22d47b692
Diego Campos <diegodiko05@gmail.com>
2020-09-17 12:46:15 -0300

    Html footer inicio do posicionamento css

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,24 +1,75 @@
 import React from 'react';
 import './style.css';
+import american from '../../images/american.png';
+import banco3 from '../../images/banco3.png';
+import cadeado from '../../images/cadeado.png';
+import groupSocial from '../../images/groupSocial.png';
+import visa from '../../images/visa.png';
+import master from '../../images/master.png';
+import boleto from '../../images/boleto.png';
 
 const Footer = () => {
   return (
     <div className="footerContainer">
       <div className="containerSuperior">
-        <div className="logoSocial"></div>
-        <div className="institucional"></div>
-        <div className="institucional"></div>
-        <div className="loja"></div>
-        <div className="atendimento"></div>
-        <div className="newsletter"></div>
+        <div className="logoSocial">
+          <img src={groupSocial} alt="logo" />
+        </div>
+        <div className="institucional">
+          <p className="titulosFooter">institucional</p>
+          <p className="paragrafoSuperior">Sobre Nós</p>
+          <p className="paragrafoSuperior">Contato</p>
+        </div>
+        <div className="loja">
+          <p className="titulosFooter">Loja</p>
+          <p className="paragrafoSuperior">Cadastre-se</p>
+          <p className="paragrafoSuperior">Área do lojista</p>
+
+        </div>
+        <div className="atendimento">
+          <p className="titulosFooter">Atendimento</p>
+          <p className="paragrafoSuperior">hello@espaconamah.com.br</p>
+          <p className="paragrafoSuperior">(31) 2515 6200</p>
+
+        </div>
+        <div className="newsletter">
+          <p className="titulosFooter">Assine nossa Newsletter</p>
+          <div className="containerInput">
+            <input className="inputFooter" type="e-mail" placeholder="Seu endereço de e-mail" />
+            <button type="button">ASSINAR</button>
+          </div>
+        </div>
       </div>
-      <hr></hr>
+      <hr />
       <div className="containerInferior">
-        <div className="cnpj"></div>
-        <div className="pagamento"></div>
-        <div className="siteSeguro"></div>
-        <div className="FAQ"></div>
-        <div className="bigBang"></div>
+        <div className="cnpj">
+          <p className="paragrafoInferior">
+            © 2020 Espaço Namah. Todos os direitos reservados. CNPJ: 19.035.854/0001-31.
+          </p>
+        </div>
+        <div className="pagamento">
+          <p className="paragrafoInferior">Formas de pagamento:</p>
+          <img src={visa} alt="visa" />
+          <img src={master} alt="mastercard" />
+          <img src={banco3} alt="Dinners" />
+          <img src={american} alt="American Express" />
+          <img src={boleto} alt="Boleto bancario" />
+        </div>
+        <div className="siteSeguro">
+          <img src={cadeado} alt="Site Seguro" />
+          <p className="paragrafoInferior">Site seguro</p>
+        </div>
+        <div className="FAQ">
+          <a className="linkFooter" href="/">
+            Dúvidas Frequentes | Políticas da Loja
+          </a>
+        </div>
+        <div className="bigBang">
+          <p className="paragrafoInferior">
+            Site por
+            <strong>BIG BANG SHOP</strong>
+          </p>
+        </div>
       </div>
     </div>
   );
